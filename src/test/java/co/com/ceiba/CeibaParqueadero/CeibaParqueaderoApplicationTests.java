44f392cfcbdc0cd0e5bcd285d8cd1c861bbc546e
Alejandro Isaza Delgado <hildyale@gmail.com>
2018-11-28 06:59:25 -0500

    probando tests

--- src/test/java/co/com/ceiba/CeibaParqueadero/CeibaParqueaderoApplicationTests.java
+++ src/test/java/co/com/ceiba/CeibaParqueadero/CeibaParqueaderoApplicationTests.java
@@ -6,9 +6,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import co.com.ceiba.CeibaParqueadero.CeibaParqueaderoApplication;
 
-/*@RunWith(SpringRunner.class)
-@SpringBootTest*/
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CeibaParqueaderoApplication.class)
 public class CeibaParqueaderoApplicationTests {
 
 	@Test
